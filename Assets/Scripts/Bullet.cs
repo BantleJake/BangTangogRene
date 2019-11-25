e3bebefb3dfe6282ad69d43720499af5109bbd00
BantleJake <57186752+BantleJake@users.noreply.github.com>
2019-11-25 13:44:06 +0100

    Lidt mere arbejde på skyderi og flere mapper

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float projSpeed;
+    private Rigidbody2D theRB;
+
+    public GameObject bulletEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(projSpeed * transform.localScale.x * 5, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(bulletEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player1Con.cs
+++ Assets/Scripts/Player1Con.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Player1Con : MonoBehaviour
 {
 
+    //Variable der bestemmer hop og bevægelseshastighed
     public float movSpeed;
     public float jumpForce;
 
+    //En bool der checker om spilleren står på jorden
     private bool jumpCheck;
 
+    //Custom controls der kan sættes i editoren
     public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
     public KeyCode shoot;
 
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,6 +29,7 @@ public class Player1Con : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Basalt movement system hvor vi bruger hastighedsvektorer
         if (Input.GetKey(left))
         {
             rb.velocity = new Vector2(-movSpeed, rb.velocity.y);
@@ -36,24 +41,25 @@ public class Player1Con : MonoBehaviour
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
+        //Hoppe system, der checker om man trykker på hoppe knappen, og om man står på jorden, før man kan hoppe.
         if (Input.GetKey(jump) && jumpCheck == false)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
 
+    //Her checker vi bare om vi står på jorden så vi kan hoppe.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         jumpCheck = false;
-        //hasJumped = false;
-        print("Landet");
+        
 
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         jumpCheck = true;
-        print("Hop");
+        
 
     }
 }
