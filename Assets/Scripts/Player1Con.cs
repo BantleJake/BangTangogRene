1b1a1a810f2043aa7b7015df5020b1f29e545350
BantleJake <57186752+BantleJake@users.noreply.github.com>
2019-12-06 13:14:13 +0100

    Dekoreret bane med møbler og alting er sat op med splat og platforme

--- Assets/Scripts/Player1Con.cs
+++ Assets/Scripts/Player1Con.cs
@@ -19,25 +19,32 @@ public class Player1Con : MonoBehaviour
     public KeyCode jump;
     public KeyCode shoot;
 
+    //Variable til at skyde med ketchup pistolen
     public GameObject throwPoint;
     public GameObject bullet;
     private Rigidbody2D rb;
     private bool gun1ShotLimit;
     public bool haveGun1;
 
+    //Modstanderens gameobjekt vi bruger til at forhindre dem i at kollidere
     private GameObject opponent;
+
+    //Den originale position til vores spillere, når vi skal genstarte runden
     private Vector3 originalePos;
     
 
     void Start()
     {
+
+        //Når scenen starter definerer vi disse variable så vi har gemt deres værdi til senere.
         rb = GetComponent<Rigidbody2D>();
-        
         gun1ShotLimit = true;
         originalePos = gameObject.transform.position;
         haveGun1 = false;
 
-
+        //Dette er et "for each" loop, hvor vi leder efter alle gameobjekter med tagget "player". Derefter vælger vi det objekt
+        //der ikke er denne spiller, og sætter det til at være modstanderen. Så er vi altid sikre på vi kan finde en modstander, så længe
+        //der er en i scenen.
        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
         {
             if (player != this)
@@ -45,21 +52,26 @@ public class Player1Con : MonoBehaviour
                 opponent = player;
             }
         }
-
+        //Nu hvor vi har en modstander kan vi fortælle unity at den skal ignorerer collisionen mellem disse.
         Physics2D.IgnoreCollision(opponent.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Basalt movement system hvor vi bruger hastighedsvektorer
+        //Basalt movement system hvor vi bruger hastighedsvektorer. Hvis vi holder en knap nede, vil vi bevæge i den retning, ellers vil vi stoppe.
+        //Systemet sørger for at al vertical momentum er bibeholdt.
         if (Input.GetKey(left))
         {
             rb.velocity = new Vector2(-movSpeed, rb.velocity.y);
+
+            //Her vender vi gameobjektet baseret på retningen, så animation er rigtig.
             transform.localScale = new Vector3 (0.1f,0.1f,1f);
         } else if (Input.GetKey(right))
         {
             rb.velocity = new Vector2(movSpeed, rb.velocity.y);
+
+            //Her vender vi gameobjektet baseret på retningen, så animation er rigtig.
             transform.localScale = new Vector3(-0.1f, 0.1f, 1f);
         } else
         {
@@ -67,23 +79,32 @@ public class Player1Con : MonoBehaviour
         }
 
         //Hoppe system, der checker om man trykker på hoppe knappen, og om man står på jorden, før man kan hoppe.
+        //Systemet fungerer ved at en lille kasse under spilleren konstant checker om man står på jorden. Hvis kassen rør jorden, så kan man hoppe.
         if (Input.GetKeyDown(jump) && jumpCheck == false)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            print("hop");
+            
             
         }
+        
 
+        //Det er her vi skyder med Ketchup gunnen. Vi spawner en klon af vores bullet, udfra et gameobjekt vi har placeret på vores spiller.
+        //Systemet checker om vi har en ketchupgun, om vi trykker på skyde knappen, og om vi lige har skudt. 
         if (Input.GetKeyDown(shoot) && gun1ShotLimit == true && haveGun1 == true)
         {
+            //Vi sætter en bool til falsk, så vi ikke kan skyde.
             gun1ShotLimit = false;
             GameObject bulletClone = (GameObject)Instantiate(bullet, throwPoint.transform.position, throwPoint.transform.rotation);
             bulletClone.transform.localScale = -transform.localScale * 2f;
+            //Vi får en funktion til at sætte samme bool til sand efter et sekund, så vi kan skyde igen.
             Invoke("ShootAgain", 1f);
         }
     }
 
-    //Her checker vi bare om vi står på jorden så vi kan hoppe.
+    //Her checker vi de forskellige ting der kollider og hvad de gør.
+    //Hvis vores lille bobble rammer jorden, så kan man hoppe.
+    //Hvis vi bliver ramt af en bullet, så bliver vores spiller destrueret.
+    //Hvis vi kolliderer med ketchupgun, så har vi den
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -103,6 +124,7 @@ public class Player1Con : MonoBehaviour
 
     }
 
+    //Hvis vores lille hoppe-bobbel forlader jorden, så kan vi ikke hoppe igen.
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -111,6 +133,7 @@ public class Player1Con : MonoBehaviour
         }
     }
 
+    //Funktion vi bruger til at forsinke vores skud-frekvens.
     void ShootAgain()
     {
         gun1ShotLimit = true;
