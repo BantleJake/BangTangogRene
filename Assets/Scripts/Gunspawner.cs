85edb5aee8cd6c3a359f3c1c72a0dbdb4eff2613
BantleJake <57186752+BantleJake@users.noreply.github.com>
2020-01-03 16:25:38 +0100

    Fixet Ketchup respawning

---
+++ Assets/Scripts/Gunspawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunspawner : MonoBehaviour
+{
+
+    public GameObject GunPrefab;
+    public GameObject Gun;
+    private bool GunTimeDone = true;
+
+
+    Vector3 Gunpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gunpos = Gun.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gun == null && GunTimeDone)
+        {
+            GunTimeDone = false;
+            Timer();
+        }
+    }
+
+    void Timer()
+    {
+        Invoke("SpawnGun", 5f);
+    }
+
+   void SpawnGun()
+    {
+
+        Gun = Instantiate(GunPrefab, Gunpos, Quaternion.identity);
+        GunTimeDone = true;
+    }
+}
