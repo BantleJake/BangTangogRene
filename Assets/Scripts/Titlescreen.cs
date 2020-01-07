d3e0808b108d7e75bb688e49b091b51c7f8a4ca3
BantleJake <57186752+BantleJake@users.noreply.github.com>
2020-01-07 13:06:26 +0100

    Titlescreeen sat op

---
+++ Assets/Scripts/Titlescreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titlescreen : MonoBehaviour
+{
+
+    public GameObject one;
+    public GameObject two;
+    public float animTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ImageTwo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Jakob");
+        }
+    }
+
+    void ImageOne()
+    {
+        one.SetActive(true);
+        two.SetActive(false);
+        Invoke("ImageTwo", animTime);
+    }
+
+    void ImageTwo()
+    {
+        two.SetActive(true);
+        one.SetActive(false);
+        Invoke("ImageOne", animTime);
+    }
+}
